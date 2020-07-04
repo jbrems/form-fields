fb37ce94a51667addd72b52223988d4528cb0675
Jonas Brems <jonasbrems@gmail.com>
2020-07-04 12:07:24 +0200

    initial commit pt2

---
+++ projects/form-fields/src/lib/form-fields.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { FormFieldsComponent } from './form-fields.component';
+
+
+
+@NgModule({
+  declarations: [FormFieldsComponent],
+  imports: [
+  ],
+  exports: [FormFieldsComponent]
+})
+export class FormFieldsModule { }
